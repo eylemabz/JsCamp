d65c47c3d17759c12ee28973b726550a2ae9aa83
Eylem Abuzeyitoğlu <65983109+eylemabz@users.noreply.github.com>
2021-12-06 16:35:46 +0300

    JS temelleri

--- src/functions.js
+++ src/functions.js
@@ -1,103 +1,86 @@
-function addToCart(quantity,productName="Elma") {
-    console.log("Sepete eklendi : ürün : " 
-    + productName + " adet : " + quantity)
+function addToCard(quantity,productName="elma "){
+    console.log("Sepete Eklendi=> ürün:"+productName+"adet: "+quantity)
 }
 
-//addToCart()
-addToCart(10)
-//addToCart(15)
+//addToCard()
+addToCard(10)
+//addToCard(15)
 
-let sayHello = ()=>{
-    console.log("Hello World!")
+let sayHello=()=>{
+    console.log("hello World!")
 }
 
 sayHello()
 
-let sayHello2 = function () {
-    console.log("Hello World 2")
+let sayHello2=function(){
+    console.log("Hello world 2")
 }
 
 sayHello2();
 
-function addToCart2(productName, quantity, unitPrice) {
-    
+let product1={productName:"Elma",unitPrice:10,quantity:5}
+
+function addToCart(product){
+    console.log("Ürün: "+product.productName)
+    console.log("Fiyat: "+product.unitPrice)
+    console.log("Adet: "+product.quantity)
 }
 
-addToCart2("Elma",5,10)
-addToCart2("Armut", 2, 20)
-addToCart2("Limon",3,15)
-
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
-
-function addToCart3(product) {
-    console.log("Ürün : "+product.productName)
-    console.log("Adet : "+product.quantity)
-    console.log("Fiyat : "+product.unitPrice)
-}
+addToCart(product1)
 
 
-addToCart3(product1)
+let product2={productName:"Elma",unitPrice:10,quantity:5}
+let product3={productName:"Elma",unitPrice:10,quantity:5}
 
-let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-product2 = product3
-product2.productName = "KARPUZ"
+product2=product3
+product2.productName="Karpuz"
 console.log(product3.productName)
 
-let sayi1 = 10
-let sayi2 = 20
-sayi1 = sayi2
-sayi2 = 100
-console.log(sayi1)
-
-function addToCart4(products) {
-    console.log(products)
+function addToCart2(products){
+      console.log(products)
 }
 
-let products = [
-    {productName:"Elma", unitPrice:10, quantity:5},
-    {productName:"Armut", unitPrice:10, quantity:5},
-    {productName:"Karpuz", unitPrice:10, quantity:5}
+let products =[
+    {productName:"Elma",unitPrice:10,quantity:5},
+    {productName:"Armut",unitPrice:10,quantity:5},
+    {productName:"Karpuz",unitPrice:10,quantity:5}
 ]
 
-addToCart4(products)
+addToCart2(products)
 
-function add(bisey,...numbers) { //rest
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        total = total + numbers[i]
+function add(...numbers){//fonk içinde değişkenin önüne 3 noktaya rest operatörü diyoruz
+    let total=0;
+    for(let i=0;i<numbers.length;i++){
+        total=total+numbers[i]
     }
     console.log(total)
-    console.log(bisey)
 }
 
 add(20,30)
-//add(20,30, 40)
-//add(20,30, 40, 50)
+add(20,30,50)
+add(20,30,40,50)
 
-let numbers = [30,10,500,600,120]
+let numbers=[30,10,500,600,120]
 //console.log(...numbers)
 console.log(Math.max(...numbers))
 
-let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
-    {name:"İç Anadolu", population:"20M"},
-    {name:"Marmara", population:"30M"},
-    {name:"Karadeniz", population:"10M"},
+let [icAnadolu,marmara,karadeniz,[icAnadolusehirleri]]=[
+    {name:"İç Anadolu",population:"20M"},
+    {name:"Marmara",population:"30M"},
+    {name:"Karadeniz",population:"10M"},
     [
         ["Ankara","Konya"],
         ["İstanbul","Bursa"],
         ["Sinop","Trabzon"],
     ]
 ]
+//console.log(icAnadolu.name)
+//console.log(marmara.name)
+console.log(icAnadolusehirleri)
 
-// console.log(icAnadolu.name)
-// console.log(icAnadolu.population)
-console.log(icAnadoluSehirleri)
+let newproductName,newunitPrice,newquantity
+({productName:newproductName,unitPrice:newunitPrice,quantity:newquantity}={ productName:"Elma",unitPrice:10,quantity:5})
 
-let newProductName, newUnitPrice, newQuantity
-({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-= {productName:"Elma", unitPrice:10, quantity:5})
-
-console.log(newProductName)
-console.log(newUnitPrice)
-console.log(newQuantity)
+console.log(newproductName)
+console.log(newunitPrice)
+console.log(newquantity)
